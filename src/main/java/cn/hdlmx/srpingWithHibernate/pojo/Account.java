9d1fd30744b3eb7a368764839e1356d171c3f25d
handong <hdlmx@outlook.com>
2017-06-02 23:02:39 +0800

    Spring整合Hibernate --->通过IDEA生产hibernate的hbm映射文件

---
+++ src/main/java/cn/hdlmx/srpingWithHibernate/pojo/Account.java
@@ -0,0 +1,40 @@
+package cn.hdlmx.srpingWithHibernate.pojo;
+
+public class Account {
+    private Integer id;
+    private String name;
+    private int balance;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
